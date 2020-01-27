ad7f5df4fe7998d0dec3219c8fdc2c634192d63e
alfianjau <alfianjauhari@gmail.com>
2020-01-27 15:03:24 +0700

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,100 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    'nuxt-webfontloader',
+    'nuxt-purgecss'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /* Google fonts */
+  webfontloader: {
+    custom: {
+      families: ['Open Sans:n3,n4', 'Roboto:n3,n7'],
+      urls: [
+        // for each Google Fonts add url + options you want
+        // here add font-display option
+        'https://fonts.googleapis.com/css?family=Open+Sans:300,400&display=swap',
+        'https://fonts.googleapis.com/css?family=Roboto:300,700&display=swap'
+      ]
+    }
+  },
+  /* Purge CSS */
+  purgeCSS: {
+    //enabled: ({ isDev, isClient }) => !isDev && isClient, // or `false` when in dev/debug mode
+    enabled: process.env.NODE_ENV == 'production' ? true : false,
+    paths: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js'
+    ],
+    styleExtensions: ['.css'],
+    whitelist: ['body', 'html', 'nuxt-progress'],
+    extractors: [
+      {
+        extractor: class {
+          static extract(content) {
+            return content.match(/[A-z0-9-:\\/]+/g)
+          }
+        },
+        extensions: ['html', 'vue', 'js']
+      }
+    ]
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extractCSS: true,
+    extend(config, ctx) {}
+  }
+}
