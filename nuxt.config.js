fb9c60ea8758a69b36c4637dd4fa5f136d54b0ba
alfianjau <alfianjauhari@gmail.com>
2020-01-29 14:50:57 +0700

    reroll into gh branch

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: '/hakooi.com/'
+          base: '/hakooi.github.io/'
         }
       }
     : {}
