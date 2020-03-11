a1719575ba93e41d2e498154ac44823fc6910cf6
alfianjau <alfianjauhari@gmail.com>
2020-03-11 16:01:40 +0700

    remove ghpage routerbase;

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,16 @@
 /* nuxt.config.js */
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
-const routerBase =
+/* const routerBase =
 	process.env.DEPLOY_ENV === 'GH_PAGES'
 		? {
 				router: {
 					base: '/hakooi.github.io/'
 				}
 		  }
-		: {}
+		: {} */
 
 export default {
-	...routerBase,
+	// ...routerBase,
 	mode: 'universal',
 	/*
 	 ** Headers of the page
