d66aa0b97a388b6d332bd012d8e15ec363c8744f
alfianjau <alfianjauhari@gmail.com>
2020-03-13 14:57:50 +0700

    add new favicon packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,13 +20,38 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ name: 'apple-mobile-web-app-title', content: 'Hakooi' },
+			{ name: 'application-name', content: 'Hakooi' },
+			{ name: 'msapplication-TileColor', content: '#ffc40d' },
+			{ name: 'theme-color', content: '#3341ff' },
 			{
 				hid: 'description',
 				name: 'description',
 				content: process.env.npm_package_description || ''
 			}
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png'
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png'
+			},
+			{ rel: 'manifest', href: '/site.webmanifest' },
+			{ rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#3341ff' },
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-touch-icon.png'
+			}
+		]
 	},
 	/*
 	 ** Customize the progress-bar color
