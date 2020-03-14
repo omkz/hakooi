64c4f24e3b9396b8db9ad6a5ff0fff22508a8529
alfianjau <alfianjauhari@gmail.com>
2020-03-14 20:07:52 +0700

    remove some unused link on services page

--- components/Service/Service.vue
+++ components/Service/Service.vue
@@ -10,34 +10,11 @@
 						Web Application
 					</h3>
 					<p class="text-gray-600 mb-8">
-						We specialize in the scalable web applications with complex and rich functionality
-						using Ruby on Rails, Javascript, and related technologies.
-				        We can handle heavy loads and are happy to undertake the solution of complex problems.
+						We specialize in the scalable web applications with complex and rich
+						functionality using Ruby on Rails, Javascript, and related
+						technologies. We can handle heavy loads and are happy to undertake
+						the solution of complex problems.
 					</p>
-					<p class="uppercase text-white md:mb-6">Links</p>
-					<ul class="list-reset mb-6">
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline text-gray-800 hover:text-hakooi-800"
-								>FAQ</a
-							>
-						</li>
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline text-gray-800 hover:text-hakooi-800"
-								>Help</a
-							>
-						</li>
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline text-gray-800 hover:text-hakooi-800"
-								>Support</a
-							>
-						</li>
-					</ul>
 				</div>
 				<div class="flex-1 text-xl px-6">
 					<CraftIcon
@@ -47,34 +24,11 @@
 						Mobile Application
 					</h3>
 					<p class="text-gray-600 mb-8">
-						We help our clients deliver world-class, beautiful, engaging iOS and Android applications. 
-						Our team knows these platforms inside out. Depending on your requirements, 
-						it can be a native iOS/Android, a cross-platform, or a hybrid applications.
+						We help our clients deliver world-class, beautiful, engaging iOS and
+						Android applications. Our team knows these platforms inside out.
+						Depending on your requirements, it can be a native iOS/Android, a
+						cross-platform, or a hybrid applications.
 					</p>
-					<p class="uppercase text-white md:mb-6">Legal</p>
-					<ul class="list-reset mb-6">
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline hover:text-hakooi-800"
-								>Brand identity</a
-							>
-						</li>
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline hover:text-hakooi-800"
-								>ruby on rails custom site</a
-							>
-						</li>
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline hover:text-hakooi-800"
-								>Marketing landing page</a
-							>
-						</li>
-					</ul>
 				</div>
 				<div class="flex-1 text-xl px-6">
 					<BrandingIcon
@@ -85,34 +39,12 @@
 						UI/UX Design
 					</h3>
 					<p class="text-gray-600 mb-8">
-						We're not only create beautiful designs, but also highly usable, intuitive and simple-to-use.
-						Our attention to detail and UX design expertise is what makes us one of the top UX design firms in Indonesia. 
-						Together, we’ll improve the usability and usefulness of your applications for an optimal user experience.
+						We're not only create beautiful designs, but also highly usable,
+						intuitive and simple-to-use. Our attention to detail and UX design
+						expertise is what makes us one of the top UX design firms in
+						Indonesia. Together, we’ll improve the usability and usefulness of
+						your applications for an optimal user experience.
 					</p>
-					<p class="uppercase text-white md:mb-6">Social</p>
-					<ul class="list-reset mb-6">
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline hover:text-hakooi-800"
-								>Facebook</a
-							>
-						</li>
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline hover:text-hakooi-800"
-								>Linkedin</a
-							>
-						</li>
-						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-							<a
-								href="#"
-								class="transitions duration-300 ease-in underline hover:text-hakooi-800"
-								>Twitter</a
-							>
-						</li>
-					</ul>
 				</div>
 			</div>
 		</div>
