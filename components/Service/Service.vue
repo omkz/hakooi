ec8078c67794991c409c1c12f1f19506584e3283
alfianjau <alfianjauhari@gmail.com>
2020-03-14 20:11:38 +0700

    add padding top

--- components/Service/Service.vue
+++ components/Service/Service.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="services bg-hakooi">
-		<div class="mx-auto container">
+		<div class="mx-auto container md:pt-12">
 			<div class="flex md:flex-row flex-col text-white ">
 				<div class="flex-1 text-xl px-6">
 					<NetworkIcon
