eefb6d8bda8e5244f6210c23fbaf8171c9b2fe6b
alfianjau <alfianjauhari@gmail.com>
2020-03-16 21:25:27 +0700

    testing logo and hero redesigning

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -2,32 +2,52 @@
 	<section class="home bg-hakooi">
 		<div class="mx-auto container md:h-screen flex flex-wrap flex-col py-6 ">
 			<!--Left Col-->
-			<div class="flex justify-end">
+			<div class="flex justify-start">
 				<div class="hakooi-image px-6 pb-2/3 h-full sm:w-full md:w-auto">
 					<!-- <img
 							alt="hakooi-logo"
 							class="h-48 w-full object-contain"
 							src="~/static/images/hakooi-logo.png"
 					/>-->
-					<picture>
+					<!-- <picture>
 						<img
 							class="h-auto w-1/2 sm:w-3/4 m-auto object-contain"
 							alt="hakooi-logo"
 							src="~/assets/images/hakooi-logo.webp"
 						/>
-					</picture>
+					</picture> -->
+					<HakooiLogo class="w-24 h-24" />
 				</div>
 			</div>
-			<div
-				class="w-full leading-relaxed text-4xl md:text-6xl justify-center text-center md:text-left"
-			>
-				<!-- <p class="uppercase tracking-loose w-full">Premium Ruby on Rails Website Studio</p> -->
-				<h3 class="font-bold px-6 text-white">Hakooi</h3>
-				<h3 class="font-bold px-6 text-white">Premium Web design studio</h3>
-				<h3 class="font-bold px-6 text-white">Ruby on Rails Crafter</h3>
-				<!-- <p class="leading-normal text-2xl mb-8">Creative and Freethinking made up our crafts</p> -->
-			</div>
+			<transition>
+				<div
+					class="w-full leading-relaxed text-4xl md:text-6xl justify-center text-center"
+				>
+					<h3 class="font-bold px-6 text-white">Hakooi</h3>
+					<h3 class="font-bold px-6 text-white">Premium Web design studio</h3>
+					<h3 class="font-bold px-6 text-white">Ruby on Rails Crafter</h3>
+				</div>
+			</transition>
+
 			<!--Right Col-->
 		</div>
 	</section>
 </template>
+<script>
+import HakooiLogo from '@/assets/svg/hakooi-logo-white.svg'
+export default {
+	components: {
+		HakooiLogo
+	}
+}
+</script>
+<style scoped>
+.svg-icon {
+	stroke-dasharray: 10;
+	stroke-dashoffset: 10;
+
+	g {
+		fill: none;
+	}
+}
+</style>
