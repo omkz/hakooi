3750e9529891f1c38dfd7de775359b40fc7405e0
Kurnia Muhamad <omenkzz@gmail.com>
2020-03-19 19:29:08 +0700

    Update facebook page link

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -83,7 +83,7 @@
 					<ul class="list-reset mb-16">
 						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
 							<a
-								href="https://www.facebook.com/Hakooi-Studio-111255730493012"
+								href="https://www.facebook.com/Hakooi-Studio"
 								class="transitions duration-300 ease-in underline hover:text-hakooi-600"
 								>Facebook</a
 							>
