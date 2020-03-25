52848ea083dfae6faa23eba395626355095f9cee
Kurnia Muhamad <omenkzz@gmail.com>
2020-03-25 19:38:36 +0700

    Update techstack

--- components/Stack/TechStack.vue
+++ components/Stack/TechStack.vue
@@ -7,7 +7,7 @@
 			<p
 				class="uppercase text-white md:text-center leading-relaxed text-3xl font-bold mb-12"
 			>
-				Our swiss knife to manifest your digital products
+				Our swiss knife to manifest your products
 			</p>
 			<div class="flex flex-wrap flex-row text-white mt-12">
 				<div class="tech-logo w-1/2 md:w-1/4 p-6">
